5f7a804ecf2a917c397d8094bd8e67a3530fffa8
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2023-06-29 18:47:44 -0300

    handle with variables comparing

--- divination.py
+++ divination.py
@@ -4,6 +4,13 @@ print("*********************************")
 
 secret_number = 42
 
-guessed_number = input('Digite um número: ')
+guessed_number = int(input('Digite um número: '))
 
 print('Você digitou: ', guessed_number)
+
+if (secret_number == guessed_number):
+  print('Você acertou!')
+else:
+  print('Você errou!')
+
+print('Fim do jogo!')
