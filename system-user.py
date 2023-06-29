78211b8f94fe9b21ae85eda57ba08d5cdebb3f8d
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2023-06-29 19:01:54 -0300

    add system-user case

---
+++ system-user.py
@@ -0,0 +1,10 @@
+user = input('Informe o usuário do sistema: ')
+
+if (user == 'Flávio'):
+  print('Seja bem-vindo Flávio!')
+elif (user == 'Douglas'):
+  print('Seja bem-vindo Douglas!')
+elif (user == 'Nico'):
+  print('Seja bem-vindo Nico')
+else:
+  print('Usuário não identificado!')
