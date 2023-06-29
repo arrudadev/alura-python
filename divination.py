1d3b770ceb42aee56d87a2a123d5079576417f5d
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2023-06-29 18:54:24 -0300

    add a tip to warning if the guessed number are bigger or less than secret number

--- divination.py
+++ divination.py
@@ -10,7 +10,9 @@ print('Você digitou: ', guessed_number)
 
 if (secret_number == guessed_number):
   print('Você acertou!')
+elif (guessed_number > secret_number):
+  print('Você errou! o seu chute foi maior que o número secreto.')
 else:
-  print('Você errou!')
+  print('Você errou! o seu chute foi menor que o número secreto.')
 
 print('Fim do jogo!')
