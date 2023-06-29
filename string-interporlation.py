e577c25057fc9105b50031dc4c53d055dc95a0b9
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2023-06-29 19:19:27 -0300

    add string interporlation case

---
+++ string-interporlation.py
@@ -0,0 +1,12 @@
+print('===============')
+print('Olá Sr.{1} {0}'.format('Cordeiro', 'Leonardo'))
+print('===============')
+print('R$ {:7.1f}'.format(1000.12))
+print('R$ {:07.2f}'.format(4.11))
+print('===============')
+
+current_attempt = 2
+attempts_total = 10
+
+print(f'Tentativa {current_attempt} de {attempts_total}')
+print('===============')
