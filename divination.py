0c2420a4cd4efcdac19a12549d2bfc9b07335f7f
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2023-06-29 20:43:06 -0300

    change the divination.py to be a module

--- divination.py
+++ divination.py
@@ -1,47 +1,49 @@
 from random import randrange
 
-print("*********************************")
-print("Bem vindo ao jogo de Adivinhação!")
-print("*********************************")
 
-# generate random number in range 1:100
-secret_number = randrange(1, 101)
-attempts_total = 0
-points = 1000
+def play():
+  print("*********************************")
+  print("Bem vindo ao jogo de Adivinhação!")
+  print("*********************************")
 
-print('Qual o nível de dificuldade?')
-print('(1) Fácil (2) Médio (3) Difícil')
+  # generate random number in range 1:100
+  secret_number = randrange(1, 101)
+  attempts_total = 0
+  points = 1000
 
-difficultyLevel = int(input('Defina o nível: '))
+  print('Qual o nível de dificuldade?')
+  print('(1) Fácil (2) Médio (3) Difícil')
 
-if (difficultyLevel == 1):
-  attempts_total = 20
-elif (difficultyLevel == 2):
-  attempts_total = 10
-else:
-  attempts_total = 5
+  difficultyLevel = int(input('Defina o nível: '))
 
-for current_attempt in range(1, attempts_total + 1):
-  print('Tentativa {} de {}'.format(current_attempt, attempts_total))
-
-  guessed_number = int(input('Digite um número entre 1 e 100: '))
-
-  print('Você digitou: ', guessed_number)
-
-  if (guessed_number < 1 or guessed_number > 100):
-    print('Você deve digitar um número entre 1 e 100!')
-    continue
-
-  if (secret_number == guessed_number):
-    print(f'Você acertou e fez {points} pontos!')
-    break
-  elif (current_attempt == attempts_total):
-    print(f'o número secreto era {secret_number}. Você fez {points} pontos!')
-  elif (guessed_number > secret_number):
-    print('Você errou! o seu chute foi maior que o número secreto.')
+  if (difficultyLevel == 1):
+    attempts_total = 20
+  elif (difficultyLevel == 2):
+    attempts_total = 10
   else:
-    print('Você errou! o seu chute foi menor que o número secreto.')
+    attempts_total = 5
 
-  points -= abs(secret_number - guessed_number)
+  for current_attempt in range(1, attempts_total + 1):
+    print('Tentativa {} de {}'.format(current_attempt, attempts_total))
 
-print('Fim do jogo!')
+    guessed_number = int(input('Digite um número entre 1 e 100: '))
+
+    print('Você digitou: ', guessed_number)
+
+    if (guessed_number < 1 or guessed_number > 100):
+      print('Você deve digitar um número entre 1 e 100!')
+      continue
+
+    if (secret_number == guessed_number):
+      print(f'Você acertou e fez {points} pontos!')
+      break
+    elif (current_attempt == attempts_total):
+      print(
+          f'o número secreto era {secret_number}. Você fez {points} pontos!')
+    elif (guessed_number > secret_number):
+      print('Você errou! o seu chute foi maior que o número secreto.')
+    else:
+      print('Você errou! o seu chute foi menor que o número secreto.')
+
+      points -= abs(secret_number - guessed_number)
+  print('Fim do jogo!')
