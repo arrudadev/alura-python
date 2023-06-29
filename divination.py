0ffd20788117c88f81602c015a2be0089a63f479
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2023-06-29 18:45:26 -0300

    handle with user input

---
+++ divination.py
@@ -0,0 +1,9 @@
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+secret_number = 42
+
+guessed_number = input('Digite um número: ')
+
+print('Você digitou: ', guessed_number)
