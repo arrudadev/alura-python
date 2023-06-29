b5bfc900aab0b090b406d26a4684749312256ee1
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2023-06-29 19:06:47 -0300

    add loop while

--- divination.py
+++ divination.py
@@ -3,16 +3,22 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 secret_number = 42
+attempts_total = 3
+attempts_count = 1
 
-guessed_number = int(input('Digite um número: '))
+while (attempts_count <= attempts_total):
+  print('Tentativa', attempts_count, 'de', attempts_total)
+  guessed_number = int(input('Digite um número: '))
 
-print('Você digitou: ', guessed_number)
+  print('Você digitou: ', guessed_number)
 
-if (secret_number == guessed_number):
-  print('Você acertou!')
-elif (guessed_number > secret_number):
-  print('Você errou! o seu chute foi maior que o número secreto.')
-else:
-  print('Você errou! o seu chute foi menor que o número secreto.')
+  if (secret_number == guessed_number):
+    print('Você acertou!')
+  elif (guessed_number > secret_number):
+    print('Você errou! o seu chute foi maior que o número secreto.')
+  else:
+    print('Você errou! o seu chute foi menor que o número secreto.')
+
+  attempts_count += 1
 
 print('Fim do jogo!')
