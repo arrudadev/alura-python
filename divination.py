bcd1847f17ca623dc4f5e5bb1cfd32898fad55f1
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2023-06-29 19:14:21 -0300

    add break/continue conditions in the loop

--- divination.py
+++ divination.py
@@ -7,12 +7,18 @@ attempts_total = 3
 
 for current_attempt in range(1, attempts_total + 1):
   print('Tentativa {} de {}'.format(current_attempt, attempts_total))
-  guessed_number = int(input('Digite um número: '))
+
+  guessed_number = int(input('Digite um número entre 1 e 100: '))
 
   print('Você digitou: ', guessed_number)
 
+  if (guessed_number < 1 or guessed_number > 100):
+    print('Você deve digitar um número entre 1 e 100!')
+    continue
+
   if (secret_number == guessed_number):
     print('Você acertou!')
+    break
   elif (guessed_number > secret_number):
     print('Você errou! o seu chute foi maior que o número secreto.')
   else:
