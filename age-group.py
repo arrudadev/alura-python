103a31ed1feae9ba30b00f8322f0d4880b85ed2e
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2023-06-29 18:58:20 -0300

    add age-group case

---
+++ age-group.py
@@ -0,0 +1,8 @@
+age = int(input('Digite sua idade: '))
+
+if (age >= 18):
+  print('Você é maior de idade.')
+elif (age < 12):
+  print('Você é uma criança.')
+else:
+  print('Você é um adolescente.')
