00826cf5d97cb8205a87f03fe9eb7157362e14a6
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2023-06-30 20:38:40 -0300

    add feature to ask the user kick about the secret word

--- hangman.py
+++ hangman.py
@@ -3,6 +3,19 @@ def play():
   print("Bem vindo ao jogo da Forca!")
   print("*********************************")
 
+  secret_word = 'banana'
+  hanged = False
+  hit = False
+
+  while (not hit and not hanged):
+    kick = input('Qual letra? ').strip()
+
+    index = 0
+    for letter in secret_word:
+      if (kick.upper() == letter.upper()):
+        print(f'Encontrei a letra {letter.lower()} na posição {index}')
+      index += 1
+
 
 if (__name__ == "__main__"):
   play()
