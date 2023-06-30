2ba5d14caa12dd9f8b6e8667c3d353532b931e2d
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2023-06-30 20:46:01 -0300

    add case to handle with lists

---
+++ lists.py
@@ -0,0 +1,9 @@
+values = [0, 1, 2, 3, 4]
+
+print('Type:', type(values))
+print('Min:', min(values))
+print('Max:', max(values))
+print('Find by index:', values[2])
+print('Get length:', len(values))
+print('Check if a value (0) are in the list', (0 in values))
+print('Check if a value (8) are in the list', (8 in values))
